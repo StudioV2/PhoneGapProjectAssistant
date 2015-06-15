2af617f2f0aa74f37b1e870ab05d79e21538c5f8
Duane Bekaert <duane.bekaert@gmail.com>
2015-06-15 15:11:14 +0200

    Initial commit.

---
+++ PhoneGapProjectAssistant/MainWindow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Gtk;
+
+namespace PhoneGapProjectAssistant
+{
+	public partial class MainWindow: Gtk.Window
+	{
+		public MainWindow () : base (Gtk.WindowType.Toplevel)
+		{
+			Build ();
+		}
+
+		protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+		{
+			Application.Quit ();
+			a.RetVal = true;
+		}
+
+		protected void newButtonClicked (object sender, EventArgs e)
+		{
+			var win = new NewProjectWindow ();
+			win.Show ();
+			Hide ();
+		}
+
+		protected void openButtonClicked (object sender, EventArgs e)
+		{
+			Gtk.FileChooserDialog f = 
+				new Gtk.FileChooserDialog("Choose the directory or project to open",
+					this,
+					Gtk.FileChooserAction.SelectFolder,
+					"Cancel", Gtk.ResponseType.Cancel,
+					"Open", Gtk.ResponseType.Accept);
+			var ret = f.Run ();
+			if (ret == (int)ResponseType.Accept) {
+				if (true) {
+					var project = new PhoneGapProject (f.Filename);
+					if (project.Valid) {
+						var win = new PhoneGapProjectManagerWindow (project);
+						win.Show ();
+						f.Hide ();
+						Hide ();
+					} else {
+						MessageDialog md = new MessageDialog (this, 
+							DialogFlags.DestroyWithParent, MessageType.Error, 
+							ButtonsType.Close, "This folder doesn't appear to contain a valid PhoneGap project.");
+						md.Run();
+						md.Destroy();
+						f.Hide();
+					}
+				}
+			} else {
+				f.Hide ();
+			}
+		}
+	}
+}
